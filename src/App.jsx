c6517f05988942c566c983bf6b9e6783e23a11a7
sothulthorn <thornsothul@gmail.com>
2024-07-09 21:45:50 +0900

    Implement add an item to the list

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
+import { useState } from 'react';
 import BackgroundHeading from './components/BackgroundHeading';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import ItemList from './components/ItemList';
 import Sidebar from './components/Sidebar';
+import { initialItems } from './lib/constants';
 
 function App() {
+  const [items, setItems] = useState(initialItems);
+
   return (
     <>
       <BackgroundHeading />
 
       <main>
         <Header />
-        <ItemList />
-        <Sidebar />
+        <ItemList items={items} />
+        <Sidebar setItems={setItems} />
       </main>
 
       <Footer />
--- src/components/AddItemForm.jsx
+++ src/components/AddItemForm.jsx
@@ -1,10 +1,40 @@
+import { useRef, useState } from 'react';
 import Button from './Button';
 
-const AddItemForm = () => {
+const AddItemForm = ({ setItems }) => {
+  const [itemText, setItemText] = useState('');
+  const inputRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // basic validation
+    if (!itemText) {
+      alert("Item can't be empty");
+      inputRef.current.focus();
+      return;
+    }
+
+    const newItem = {
+      id: new Date().getTime(),
+      name: itemText,
+      packed: false,
+    };
+
+    setItems((prev) => [...prev, newItem]);
+
+    setItemText('');
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Add an item</h2>
-      <input />
+      <input
+        value={itemText}
+        onChange={(e) => setItemText(e.target.value)}
+        autoFocus
+        ref={inputRef}
+      />
       <Button>Add to list</Button>
     </form>
   );
