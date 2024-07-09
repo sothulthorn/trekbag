811cd64b4c708d1caf987607129c17e265134352
sothulthorn <thornsothul@gmail.com>
2024-07-09 20:44:28 +0900

    first commit

---
+++ src/components/AddItemForm.jsx
@@ -0,0 +1,13 @@
+import Button from './Button';
+
+const AddItemForm = () => {
+  return (
+    <form>
+      <h2>Add an item</h2>
+      <input />
+      <Button>Add to list</Button>
+    </form>
+  );
+};
+
+export default AddItemForm;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,14 @@
+import AddItemForm from './AddItemForm';
+import ButtonGroup from './ButtonGroup';
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <AddItemForm />
+
+      <ButtonGroup />
+    </div>
+  );
+};
+
+export default Sidebar;
