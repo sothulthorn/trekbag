b3e56831f782d35144c1ebda0cbaf3ab08d64a6f
sothulthorn <thornsothul@gmail.com>
2024-07-09 21:19:40 +0900

    Create Item List

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,12 +1,16 @@
 import Button from './Button';
+import { secondaryButtons } from '../lib/constants';
 
 const ButtonGroup = () => {
   return (
     <section className="button-group">
-      <Button type="secondary">Mark all as complete</Button>
-      <Button type="secondary">Mark all as incomplete</Button>
-      <Button type="secondary">Reset to initial</Button>
-      <Button type="secondary">Remove all items</Button>
+      {secondaryButtons.map((text) => {
+        return (
+          <Button key={text} type="secondary">
+            {text}
+          </Button>
+        );
+      })}
     </section>
   );
 };
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,5 +1,29 @@
+import { useState } from 'react';
+import { initialItems } from '../lib/constants';
+
 const ItemList = () => {
-  return <div>ItemList</div>;
+  const [items, setItems] = useState(initialItems);
+
+  return (
+    <ul>
+      {items.map((item) => {
+        return <Item key={item.id} item={item} />;
+      })}
+    </ul>
+  );
 };
 
 export default ItemList;
+
+const Item = ({ item }) => {
+  return (
+    <li className="item">
+      <label>
+        <input type="checkbox" checked={item.packed} />
+        {item.name}
+      </label>
+
+      <button>❌</button>
+    </li>
+  );
+};
